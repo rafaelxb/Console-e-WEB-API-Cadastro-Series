e99602e6512e8f730a4f898cae5fdc2925493d90
rfxRafa <rfxbarbosa@gmail.com>
2021-09-16 17:25:01 -0300

    Inclusão de WEB API

---
+++ R.Series.Web/Controllers/RegistroController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using R.Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace R.Series.Web.Controllers
+{
+    [Route("[controller]")]
+    public class RegistroController : Controller
+    {
+        private readonly IRepositorio<Registro> _repositorioRegistro;
+
+        public RegistroController(IRepositorio<Registro> repositorioRegistro)
+        {
+            _repositorioRegistro = repositorioRegistro;
+        }
+
+        [HttpGet("")]
+        public IActionResult Lista()
+        {
+            return Ok(_repositorioRegistro.Lista().Select(r => new RegistroModel(r)));            
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Atualiza(int id, [FromBody] RegistroModel model)
+        {
+            _repositorioRegistro.Atualiza(id, model.ToRegistro());
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Exclui(int id)
+        {
+            _repositorioRegistro.Exclui(id);
+            return NoContent();
+        }
+
+        [HttpPost("")]
+
+        public IActionResult Insere([FromBody] RegistroModel model)
+        {
+            model.Id = _repositorioRegistro.ProximoId();
+            
+            Registro registro = model.ToRegistro();
+            
+            _repositorioRegistro.Insere(registro);
+            return Created("", registro);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult Consulta(int id)
+        {
+            return Ok(new RegistroModel(_repositorioRegistro.Lista().FirstOrDefault(r => r.Id == id)));
+        }
+
+    }
+}
---
+++ R.Series.Web/RegistroModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace R.Series.Web
+{
+    public class RegistroModel
+    {
+        public int Id { get; set; }
+        public Tipo Tipo { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public decimal Nota { get; set; }
+        public bool Excluido { get; set; }
+
+        public RegistroModel() {}
+
+        public RegistroModel(Registro registro)
+        {
+            Id = registro.retornaId();
+            Tipo = registro.retornaTipo();
+            Genero = registro.retornaGenero();
+            Titulo = registro.retornaTitulo();
+            Ano = registro.retornaAno();
+            Nota = registro.retornaNota();
+            Excluido = registro.retornaExcluido();
+        }
+
+        public Registro ToRegistro()
+        {
+            return new Registro(Id, Tipo, Genero, Titulo, Ano, Nota);
+        }
+
+    }
+}
